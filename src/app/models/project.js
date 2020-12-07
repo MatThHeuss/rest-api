85cf9dcc1b0a30eba024f36ffd75af076c7cace3
Matheus Alencar <58493088+MatThHeuss@users.noreply.github.com>
2020-12-06 22:14:14 -0300

    Adicionando projeto

---
+++ src/app/models/project.js
@@ -0,0 +1,31 @@
+const mongoose = require('../../database/index');
+const bcrypt = require('bcryptjs');
+
+const ProjectSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required:true,
+    },
+    description:{
+        type:String,
+        require:true,
+    },
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        require:true
+    },
+    task:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'Task',
+    }],
+    createdAt:{
+        type:Date,
+        default:Date.now,
+    },
+});
+
+
+const Project = mongoose.model('Project', ProjectSchema)
+
+module.exports = Project
